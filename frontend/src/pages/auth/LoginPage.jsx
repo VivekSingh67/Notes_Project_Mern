325911ccc8631516b2085ef9e56a1af63faaff9d
Ashish Kumar Bhargav <ashishbhargavcsemitrc2020@gmail.com>
2026-01-07 17:53:17 +0530

    second message

---
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div className="min-h-screen bg-[#222831] flex items-center justify-center p-4">
+            <div className="w-full max-w-md">
+                <div className="bg-[#393E46] rounded-2xl shadow-lg p-8">
+                    <h2 className="text-3xl font-bold text-white text-center mb-8">
+                        Login Account
+                    </h2>
+
+                    <div className="space-y-3">
+                        <div>
+                            <label className="block text-sm font-medium text-white mb-2">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                className="w-full px-4 py-3 border border-gray-300 rounded-lg  outline-none text-white"
+                                placeholder="Enter your email"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="block text-sm font-medium text-white mb-2">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                className="w-full px-4 py-3 border border-gray-300 rounded-lg  outline-none text-white"
+                                placeholder="Enter your password"
+                            />
+                        </div>
+
+                        <button className="w-full bg-[#DFD0B8] text-[#393E46] font-semibold py-3 rounded-lg transition">
+                            Login
+                        </button>
+                    </div>
+
+                    <p className="text-center text-sm text-white mt-6">
+                        Don't have an account?{' '}
+                        <a href="#" className="text-[#DFD0B8] font-medium hover:underline">
+                            Sign up
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ frontend/src/pages/auth/RegisterPage.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const RegisterPage = () => {
+    return (
+        <div className="min-h-screen bg-zinc-600 flex items-center justify-center p-4">
+            <div className="w-full max-w-md">
+                <div className="bg-[#393E46] rounded-2xl shadow-lg p-8">
+                    <h2 className="text-3xl font-bold text-white text-center mb-8">
+                        Create Account
+                    </h2>
+
+                    <div className="space-y-3">
+                        <div>
+                            <label className="block text-sm font-medium text-white mb-2">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                className="w-full px-4 py-3 border border-gray-300 rounded-lg outline-none text-white"
+                                placeholder="Enter your name"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="block text-sm font-medium text-white mb-2">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                className="w-full px-4 py-3 border border-gray-300 rounded-lg outline-none text-white"
+                                placeholder="Enter your email"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="block text-sm font-medium text-white mb-2">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                className="w-full px-4 py-3 border border-gray-300 rounded-lg outline-none text-white"
+                                placeholder="Enter your password"
+                            />
+                        </div>
+
+                        <button className="w-full bg-[#DFD0B8] text-[#393E46] font-semibold py-3 rounded-lg transition">
+                            Register
+                        </button>
+                    </div>
+
+                    <p className="text-center text-sm text-white mt-6">
+                        Already have an account?{' '}
+                        <a href="#" className="text-[#DFD0B8] font-medium hover:underline">
+                            Sign in
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterPage
